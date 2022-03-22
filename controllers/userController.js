ac5a79e68d9442559b697ba36f8f6df6684b5e03
nayeemjc <RxNayeemIMI@gmail.com>
2022-03-23 00:14:43 +0600

    implemented user info updating

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require("../models/userModel");
 const catchAsync = require("../lib/catchAsync");
+const AppError = require("../lib/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((key) => {
+        if (allowedFields.includes(key)) newObj[key] = obj[key];
+    });
+    return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
@@ -8,6 +17,33 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
         status: "success",
         results: users.length,
         data: { users }
+    });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // Point: Create error if user posts password data
+    if (req.body.password || req.body.passwordConfirm)
+        return next(
+            new AppError("Updating password is not allowed here!"),
+            400
+        );
+
+    // Point: Filtered out unwanted fields
+    const filteredBody = filterObj(req.body, "name", "email");
+
+    // Point: Update user document
+    const updatedUser = await User.findByIdAndUpdate(
+        req.user.id,
+        filteredBody,
+        {
+            new: true,
+            runValidators: true
+        }
+    ); // Important: Using findByIdAndUpdate here because if we use save() then it will trigger all the pre-save() middlewares. Where one of the pre-save() middleware requires passwordConfirm. But we don't want to pass any passwordConfirm here so to get rid of this situation we are using findByIdAndUpdate() so that the pre-save() middlewares won't be triggered!
+
+    res.status(200).json({
+        status: "success",
+        data: { user: updatedUser }
     });
 });
 
