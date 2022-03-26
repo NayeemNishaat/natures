ad78309b258baf13a838d8771710e6f260097669
nayeemjc <RxNayeemIMI@gmail.com>
2022-03-26 22:48:19 +0600

    created review model

---
+++ models/reviewModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, "Review can not be empty."]
+        },
+        rating: { type: Number, min: 1, max: 5, default: 5 },
+        createdAt: { type: Date, default: new Date() },
+        // Important: Parent referencing is better.
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Tour",
+            required: [true, "Review must belong to a tour."]
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            userSchemarequired: [true, "Review must have an author."]
+        }
+    },
+    {
+        // Note: For showing virtual properties in json and object.
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
