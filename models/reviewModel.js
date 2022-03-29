2d095a15c42cec8c4aa1369edecff9b47b5df4c4
nayeemjc <RxNayeemIMI@gmail.com>
2022-03-30 00:29:24 +0600

    calculated avg rating on create tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
     {
@@ -51,6 +52,39 @@ reviewSchema.pre(/^find/, function (next) {
     this.populate("user", "name photo");
 
     next();
+});
+
+// Chapter: Static methods
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    // Important: In static method this points to the model/method on the class not of the class.
+    const stats = await this.aggregate([
+        // Important: Returns a promise!
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: "$tour", // Note: Grouping all the matched/filtered reviews by the tour!
+                nRating: { $sum: 1 },
+                averageRating: { $avg: "$rating" }
+            }
+        }
+    ]);
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsQuantity: stats[0].nRating,
+        ratingsAverage: stats[0].averageRating
+    });
+};
+
+// Chapter: Document middleware
+// reviewSchema.pre("save", function (next) { // Important: Don't use pre here because pre-save middlewares run before saving the document. So in aggregation we will not get the latest document that is creating/saving right now. Aggregation should be done after saving the document!
+reviewSchema.post("save", function () {
+    // Remark: This points to the current document (instance of the class) but we need the model because calcAverageRatings() is a static method that only available on the class itself. (the class itself). So what can we do?
+    // Important: Accessing the model/class!
+    this.constructor.calcAverageRatings(this.tour);
+
+    // next(); Warning: Post middleware doesn't have access to next()
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
