01774bcf832f6813fa7707c4e3c87205c94c253a
nayeemjc <RxNayeemIMI@gmail.com>
2022-04-14 05:55:31 +0600

    env updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -191,21 +191,22 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     const resetToken = user.createPasswordResetToken();
     await user.save({ validateBeforeSave: false }); // Important: Point: Modified data in instance method and saving here now. For saving we have a validator to check if email and password are given. But we dont send those here. So, stopping validation, hence the document will contain the previous info.
 
-    // Part: Send it to the user email.
-    const resetURL = `${req.protocol}://${req.get(
-        "host"
-    )}/api/v1/users/reset-password/${resetToken}`;
-
-    const message = `Forgot your password? Submit a patch request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
+    // const message = `Forgot your password? Submit a patch request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
 
     // Important: We need to do more than simply sending errors to the client. Hence, using try/catch block.
 
     try {
+        // Part: Send it to the user email.
+        const resetURL = `${req.protocol}://${req.get(
+            "host"
+        )}/api/v1/users/reset-password/${resetToken}`;
         // await sendEmail({
         //     email: user.email,
         //     subject: "Password reset link! (Valid for 10m.)",
         //     message
         // });
+
+        await new Email(user, resetURL).sendPasswordReset();
 
         res.status(200).json({
             status: "success",
