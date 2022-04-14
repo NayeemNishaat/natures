c681cd108c3557ceea632393157f4fd6e37c4746
nayeemjc <RxNayeemIMI@gmail.com>
2022-04-14 15:46:56 +0600

    created booking model

---
+++ models/bookingModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+    tour: {
+        type: mongoose.Schema.objectId,
+        ref: "Tour",
+        required: [true, "Booking must belong to a tour."]
+    },
+    user: {
+        type: mongoose.Schema.objectId,
+        ref: "User",
+        required: [true, "Booking must belong to a user."]
+    },
+    // Note: Price is require because price can be changed i.e. discount!
+    price: {
+        type: Number,
+        required: [true, "Booking must have a price."]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    // Remark: For manual booking created by admin outside of stripe.
+    paid: {
+        type: Boolean,
+        default: true
+    }
+});
+
+bookingSchema.pre(/^find/, function () {
+    this.populate("user").populate("tour", "name");
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
